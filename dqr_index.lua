752f61274d6eb0c513dbf9a55877819f87f526e5
Sam Potter <sampot97@gmail.com>
2021-01-09 18:56:08 +0000

    a

--- dqr_index.lua
+++ dqr_index.lua
@@ -1,25 +1,19 @@
 -- Download and run index.lua, which will download the rest of the repro
-function DownloadFile(path)
+function DownloadFile(source, target)
     local syncOrigin = "https://raw.githubusercontent.com/DigitalQR/ComputerCraft/main/"
-    local url = syncOrigin .. path
-    print("  Downloading '" .. url .."' to '" .. path .. "'")
+    local url = syncOrigin .. source
+    print("  Downloading '" .. url .."' to '" .. target .. "'")
 
     local content = http.get(url).readAll()
-    local handle = fs.open(path, "w")
+    local handle = fs.open(target, "w")
     handle.write(content)
     handle.close()
     write("  Done.")
 end
 
-syncList = {}
-syncCounter = 0
-
-syncList[syncCounter] = "DQR/inv.lua"
-syncCounter = syncCounter + 1
-
-for i=0,syncCounter do
-    DownloadFile(syncList[i])
-end
+fs.delete("DQR");
+DownloadFile("DQR/inv.lua", "DQR/inv")
+DownloadFile("DQR/mining.lua", "DQR/mining")
 
 print("Attempting to resync library")
 print("Finished syncing")
